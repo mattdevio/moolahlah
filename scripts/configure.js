173b2363245c2e34f49ac5c8b6f13812bc871599
Matt Greenberg <magreenberg@fullsail.edu>
2018-09-05 02:09:22 -0600

    Started setting up express framework and created a script to configure the .env file

---
+++ scripts/configure.js
@@ -0,0 +1,36 @@
+/* eslint no-console: 0 */
+
+/*----------  Vendor Imports  ----------*/
+const dotenv = require('dotenv');
+
+/*----------  Node Imports  ----------*/
+const fs = require('fs');
+const path = require('path');
+
+// Load the example env file
+const envConfig = dotenv.config({ path: path.resolve(__dirname, '../example.env') }).parsed;
+
+// Merge example env file with parameters
+process.argv.slice(2).forEach(keyValuePair => {
+  const kvp = keyValuePair.split('=');
+  if (kvp[0] in envConfig) {
+    envConfig[kvp[0]] = kvp[1];
+  } else {
+    throw new Error(`Parameter key [${kvp[0]}] doesn't exist in the 'example.env' file.`);
+  }
+});
+
+// Build config file as a string
+let envFile = '';
+Object.keys(envConfig).forEach(key => {
+  envFile += `${key}=${envConfig[key]}\n`;
+});
+
+// Show some feedback!
+console.log('Writing .env File ✏️\n');
+console.log(envFile);
+
+// Write File
+fs.writeFile(path.resolve(__dirname, '../.env'), envFile, 'utf8', () => {
+  console.log('Finished! 👍');
+});
--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log('Server File');
+/*----------  Custom Imports  ----------*/
+const expressServer = require('./bin/expressServer');
+const config = require('../config/config.js');
+
+/**
+ * Start the express server
+ */
+expressServer.listen(config.port, () => {
+  console.log(`Server is listening @ ${config.BASE_URL}:${config.PORT}`);
+});
