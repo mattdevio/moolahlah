f0e5dc6a56a08643551fe3ac711a2d52f5a63776
Matt Greenberg <magreenberg@fullsail.edu>
2018-09-08 15:24:23 -0600

    Added files for webpack and the main react front end source code

---
+++ src/entry.jsx
@@ -0,0 +1,8 @@
+/*----------  Vendor Imports  ----------*/
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <h1>React Is Working!</h1>,
+  document.getElementById('root'),
+);
