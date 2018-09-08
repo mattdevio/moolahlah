86c9597203f6768898adc6431b0d967e32b4e2e1
Matt Greenberg <magreenberg@fullsail.edu>
2018-09-08 17:02:01 -0600

    Express app now serves webpack bundles and loads from a Promise

---
+++ server/bin/initServer.js
@@ -0,0 +1,58 @@
+/*----------  Vendor Imports  ----------*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const merge = require('webpack-merge');
+
+/*----------  Node Imports  ----------*/
+const http = require('http');
+const path = require('path');
+
+/*----------  Custom Imports  ----------*/
+const commonWebpackConfig = require('../../webpack_config/webpack.common');
+const devWebpackConfig = require('../../webpack_config/webpack.dev');
+const prodWebpackConfig = require('../../webpack_config/webpack.prod');
+
+/*==================================
+=            initServer            =
+==================================*/
+
+const initServer = () => {
+  return new Promise(async (resolve, reject) => {
+
+    const app = express();
+    const expressServer = http.Server(app);
+    
+    // Add Middleware To The Express App
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(morgan('dev'));
+    
+    // Load common webpack file
+    let common;
+    try {
+      common = await commonWebpackConfig();
+    } catch (e) {
+      return reject(e);
+    }
+
+    // Flatten webpack configs and run though webpack compiler
+    const fullWebpackConfig = merge(common, (process.env.NODE_ENV === 'development') ? devWebpackConfig : prodWebpackConfig);
+    const compiler = webpack(fullWebpackConfig);
+
+    // Serve webpack bundles with webpack dev middleware
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: fullWebpackConfig.output.publicPath,
+    }));
+
+    // Resolve promise with express server
+    resolve(expressServer);
+
+  }); // end Promise
+}; // end function initServer
+
+module.exports = initServer;
+
+/*=====  End of initServer  ======*/
--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 /*----------  Load .env File First!  ----------*/
 require('dotenv').config();
 
-/*----------  Vendor Imports  ----------*/
-
-
 /*----------  Custom Imports  ----------*/
-const expressServer = require('./bin/expressServer');
+const initServer = require('./bin/initServer');
 const utility = require('./bin/utility');
 
-// Start
-expressServer.listen(process.env.PORT, () => {
-  utility.log(`Server listening on : ${process.env.BASE_URL}:${process.env.PORT}`);
-});
+/*----------  Run Server  ----------*/
+initServer()
+  .then(server => server.listen(process.env.PORT, () => {
+    utility.log(`Server listening on ${process.env.BASE_URL}:${process.env.PORT}`);
+  }))
+  .catch(e => utility.log(e));
