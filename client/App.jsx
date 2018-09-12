d4d605df57776ae8f397e4812b2971dd0a143b42
Matt Greenberg <magreenberg@fullsail.edu>
2018-09-11 22:31:23 -0600

    setup basic app file

---
+++ client/App.jsx
@@ -0,0 +1,23 @@
+/*----------  Vendor Imports  ----------*/
+import React, { Component } from 'react';
+
+/*----------  Custom Imports  ----------*/
+
+
+/*===========================
+=            App            =
+===========================*/
+
+class App extends Component {
+
+  render() {
+    return (
+      <h1>App Component</h1>
+    );
+  }
+
+}
+
+export default App;
+
+/*=====  End of App  ======*/
