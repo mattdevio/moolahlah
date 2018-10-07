2bb357a156d286eb7b19b607509771da0eb16955
Matt Greenberg <magreenberg@fullsail.edu>
2018-10-06 18:36:40 -0600

    Added retina compliant logo

--- client/screens/LandingPage.jsx
+++ client/screens/LandingPage.jsx
@@ -18,7 +18,7 @@ class LandingPage extends Component {
     } = this.props;
     return (
       <LandingPageContainer minHeight={ windowHeight }>
-        <h1>Hello World</h1>
+        <LandingPageLogo />
       </LandingPageContainer>
     );
   }
@@ -46,5 +46,11 @@ const LandingPageContainer = styled.div.attrs({
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: blue;
+`;
+
+const LandingPageLogo = styled.span`
+  background: url(/assets/image/moolahlah_logo.png) no-repeat;
+  background-size: 30rem 8.9rem;
+  width: 30rem;
+  height: 8.9rem;
 `;
