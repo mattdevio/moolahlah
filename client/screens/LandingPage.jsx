c2548b505729f8849730ec7d1bbeeba422dd4206
Matt Greenberg <magreenberg@fullsail.edu>
2018-10-06 18:26:36 -0600

    Building out landing page

--- client/screens/LandingPage.jsx
+++ client/screens/LandingPage.jsx
@@ -1,11 +1,50 @@
+/*----------  Vendor imports  ----------*/
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+/*----------  Custom imports  ----------*/
+
+
+/*==========================================
+=            LandingPage Screen            =
+==========================================*/
 
 class LandingPage extends Component {
   render() {
+    const {
+      windowHeight,
+    } = this.props;
     return (
-      <h1>HelloWorld</h1>
+      <LandingPageContainer minHeight={ windowHeight }>
+        <h1>Hello World</h1>
+      </LandingPageContainer>
     );
   }
 }
 
-export default LandingPage;
+LandingPage.propTypes = {
+  windowHeight: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = state => ({
+  windowHeight: state.ui.windowHeight,
+});
+
+export default connect(mapStateToProps)(LandingPage);
+
+/*=====  End of LandingPage Screen  ======*/
+
+const LandingPageContainer = styled.div.attrs({
+  style: ({ minHeight }) => ({
+    minHeight: `${minHeight}px`,
+  }),
+})`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: blue;
+`;
