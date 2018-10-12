f31fa3098c4b2a86eeae251a34ce3e67e4a4b154
Matt Greenberg <magreenberg@fullsail.edu>
2018-10-12 01:26:52 -0600

    Added apiResponse to the error middleware

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -3,6 +3,7 @@ const appRoot = require('app-root-path');
 
 /*----------  Custom Imports  ----------*/
 const { logger } = require(`${appRoot}/server/bin/utility`);
+const apiResponse = require(`${appRoot}/server/bin/apiResponse`);
 
 
 /*===========================================
@@ -16,11 +17,15 @@ function handleRequestErrors() {
       return next(err);
     }
 
-    logger.error(`Express middleware caught an error: ${JSON.stringify(err)}`);
+    logger.error(`Express middleware caught an error: "${err.message}" ${err.stack}`);
 
     res.status(500);
     if (process.env.NODE_ENV === 'development') {
-      res.json(err);
+      res.json(apiResponse({
+        status: 0,
+        errors: [err],
+        message: `Error from ${req.method} request to ${req.path}`
+      }));
     } else {
       res.end();
     }
