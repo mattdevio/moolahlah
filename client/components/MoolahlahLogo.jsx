e7c94017a47d1a1d4dba00b13e749977a8ede977
Matt Greenberg <magreenberg@fullsail.edu>
2018-10-20 00:39:43 -0600

    Removing duplicate logo syntax

--- client/components/MoolahlahLogo.jsx
+++ client/components/MoolahlahLogo.jsx
@@ -5,15 +5,6 @@ import styled from 'styled-components';
  * Moolahlah Logo
  * The bread & butter of the brand.
  */
-// const MoolahlahLogo = styled.span`
-//   background: url(/assets/image/moolahlah_logo.png) no-repeat;
-//   background-size: 30rem 8.9rem;
-//   width: 30rem;
-//   height: 8.9rem;
-//   margin-bottom: 2rem;
-//   display: inline-block;
-// `;
-
 const MoolahlahLogo = styled.span`
   background: url(/assets/image/moolahlah_logo.png) no-repeat;
   background-size: ${({ width }) => `${width}rem ${(+width/3.37)}rem`};
