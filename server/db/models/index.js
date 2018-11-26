6691b3595ec2453c0999d69b9aa2fbdcad7fa604
Matt Greenberg <magreenberg@fullsail.edu>
2018-11-26 04:05:18 -0700

    Adding logging serialization to knex so I could actually see the logs through the existing transports

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,12 +7,20 @@ const Knex = require('knex');
 const { readdirSync } = require('fs');
 const { basename } = require('path');
 const thisFile = basename(__filename, '.js');
+const { logger } = require(`${appRoot}/server/bin/utility`);
 
 // Custom Imports
 const knexConfig = require(`${appRoot}/knexfile`);
 
 // Initialize Knex sql builder instance
-const knex = Knex(knexConfig[process.env.NODE_ENV]);
+const knex = Knex(Object.assign({
+  log: {
+    warn: msg => logger.info(JSON.stringify(msg, null, 2)),
+    error: msg => logger.error(JSON.stringify(msg, null, 2)),
+    deprecate: msg => logger.info(JSON.stringify(msg, null, 2)),
+    debug: msg => logger.debug(JSON.stringify(msg, null, 2)),
+  }
+}, knexConfig[process.env.NODE_ENV]));
 
 // Bind all models to the knex instance
 Model.knex(knex);
@@ -22,8 +30,9 @@ Model.knex(knex);
  */
 module.exports = readdirSync(`${appRoot}/server/db/models`)
   .map(file => basename(file, '.js'))
-  .filter(file => file !== thisFile)
+  .filter(file => ((file !== thisFile) && (file !== 'BaseModel')))
   .reduce((acc, val) => {
-    acc[basename(val, '.js')] = require(`${appRoot}/se3rver/db/models/${val}`);
+    logger.info(`Loading Objection Model [${val}]`);
+    acc[basename(val, '.js')] = require(`${appRoot}/server/db/models/${val}`);
     return acc;
   }, { knex });
