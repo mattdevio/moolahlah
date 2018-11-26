db233f1d9d4eef45bc919c1a94f281a84880185b
Matt Greenberg <magreenberg@fullsail.edu>
2018-11-25 17:01:55 -0700

    Adding migration to create the user table

---
+++ server/db/migrations/20181125151732_create_user_table.js
@@ -0,0 +1,17 @@
+const tableName = 'users';
+
+exports.up = function(knex) {
+  return knex.schema.createTable(tableName, function (table) {
+    table.increments('id').primary();
+    table.string('name', 150).notNullable();
+    table.string('email', 100).notNullable();
+    table.string('password', 100).notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('last_login').defaultTo(knex.fn.now());
+    table.unique(['email']);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists(tableName);
+};
