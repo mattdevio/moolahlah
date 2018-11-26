34dee7664028e47e64cf0865deada63deb80089c
Matt Greenberg <magreenberg@fullsail.edu>
2018-11-25 19:28:51 -0700

    Working on the Models index which will work as a barrel file to load and expose all Models in the Models directory.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,9 +3,27 @@ const appRoot = require('app-root-path');
 const { Model } = require('objection');
 const Knex = require('knex');
 
+// Node Imports
+const { readdirSync } = require('fs');
+const thisFile = require('path').basename(__filename);
+
 // Custom Imports
 const knexConfig = require(`${appRoot}/knexfile`);
 
-// Setup
+// Initialize Knex sql builder instance
 const knex = Knex(knexConfig[process.env.NODE_ENV]);
+
+// Bind all models to the knex instance
 Model.knex(knex);
+
+/**
+ * Load All Models and export like a barrel file
+ */
+const exports = { knex };
+readdirSync(`${appRoot}/server/db/models`)
+  .filter(file => file !== thisFile)
+  .forEach(file => {
+    console.log(file);
+  });
+
+module.exports = exports;
