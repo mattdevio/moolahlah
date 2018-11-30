61bb65a662246a4aace5f41aa062e1de42328ac0
Matt Greenberg <magreenberg@fullsail.edu>
2018-11-30 01:04:48 -0700

    Adding comments to describe each key in the table

--- server/db/migrations/20181129021759_create_calendar_table.js
+++ server/db/migrations/20181129021759_create_calendar_table.js
@@ -2,17 +2,17 @@ const tableName = 'calendar';
 
 exports.up = function(knex) {
   return knex.schema.createTable(tableName, function(table) {
-    table.integer('id').primary();
-    table.date('db_date').notNullable();
-    table.integer('year').notNullable();
-    table.integer('month').notNullable();
-    table.integer('day').notNullable();
-    table.integer('quarter').notNullable();
-    table.integer('week').notNullable();
-    table.string('day_name', 9).notNullable();
-    table.string('month_name', 9).notNullable();
-    table.boolean('holiday_flag').defaultTo(false);
-    table.boolean('weekend_flag').defaultTo(false);
+    table.integer('id').primary();                        // year*10000 + month*100 + day = yyyymmdd
+    table.date('db_date').notNullable();                  // date string
+    table.integer('year').notNullable();                  // yyyy
+    table.integer('month').notNullable();                 // 1 to 12
+    table.integer('day').notNullable();                   // 1 to 31
+    table.integer('quarter').notNullable();               // 1 to 4
+    table.integer('week').notNullable();                  // 1 to <52 | 53>
+    table.string('day_name', 9).notNullable();            // ['Monday', 'Tuesday', ect...]
+    table.string('month_name', 9).notNullable();          // ['January', 'February', ect...]
+    table.boolean('holiday_flag').defaultTo(false);       // true or false
+    table.boolean('weekend_flag').defaultTo(false);       // true or false
     table.unique(['year', 'month', 'day']);
     table.unique('db_date');
   });
