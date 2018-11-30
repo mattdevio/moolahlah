7cdf20646314737faa5e81a1ea6308ed18973335
Matt Greenberg <magreenberg@fullsail.edu>
2018-11-29 23:47:12 -0700

    Table structure for calendar design setup in knex. Migration tested and working

--- server/db/migrations/20181129021759_create_calendar_table.js
+++ server/db/migrations/20181129021759_create_calendar_table.js
@@ -4,6 +4,17 @@ exports.up = function(knex) {
   return knex.schema.createTable(tableName, function(table) {
     table.integer('id').primary();
     table.date('db_date').notNullable();
+    table.integer('year').notNullable();
+    table.integer('month').notNullable();
+    table.integer('day').notNullable();
+    table.integer('quarter').notNullable();
+    table.integer('week').notNullable();
+    table.string('day_name', 9).notNullable();
+    table.string('month_name', 9).notNullable();
+    table.boolean('holiday_flag').defaultTo(false);
+    table.boolean('weekend_flag').defaultTo(false);
+    table.unique(['year', 'month', 'day']);
+    table.unique('db_date');
   });
 };
 
