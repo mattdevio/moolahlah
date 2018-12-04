a346571af478becf5e8b0f2a28fcc47072833bcd
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-04 01:49:08 -0700

    Making seeders atomic

--- server/db/seeds/populate_status_table.js
+++ server/db/seeds/populate_status_table.js
@@ -1,5 +1,6 @@
 exports.seed = function(knex, Promise) {
   return new Promise(async function(resolve) {
+    await knex('status').del();
     await knex('status').insert([
       { status_type: 'active' },
       { status_type: 'deleted' },
