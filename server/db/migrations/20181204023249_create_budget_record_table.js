6e1a0f4955ffc5a9124d5e049e54a87d10a91df9
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-04 02:42:15 -0700

    Setup basic structure for the budget record table

--- server/db/migrations/20181204023249_create_budget_record_table.js
+++ server/db/migrations/20181204023249_create_budget_record_table.js
@@ -3,7 +3,12 @@ const tableName = 'budget_record';
 exports.up = function(knex, Promise) {
   return new Promise(async function(resolve) {
     await knex.schema.createTable(tableName, (table) => {
-
+      table.increments('id').primary();
+      table.integer('budget_id').unsigned().notNullable();
+      table.integer('category_id').unsigned().notNullable();
+      table.string('label').notNullable();
+      table.date('estimate_date').notNullable();
+      table.decimal('estimate', 2).notNullable();
     });
     resolve();
   });
