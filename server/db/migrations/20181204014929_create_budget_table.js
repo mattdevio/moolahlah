8b230c2bb0a54f0e67a1e050d0a363a0edc9b2be
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-04 02:10:55 -0700

    finished adding a budget table with foriegn key constraints

--- server/db/migrations/20181204014929_create_budget_table.js
+++ server/db/migrations/20181204014929_create_budget_table.js
@@ -1,8 +1,18 @@
+const tableName = 'budget';
 
 exports.up = function(knex, Promise) {
-  
+  return new Promise(async function(resolve) {
+    await knex.schema.createTable(tableName, (table) => {
+      table.increments('id').primary();
+      table.binary('user_uuid', 16).notNullable();
+      table.date('start_date').notNullable();
+      table.foreign('user_uuid').references('users.uuid').onDelete('cascade');
+      table.foreign('start_date').references('calendar.db_date').onDelete('cascade');
+    });
+    resolve();
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists(tableName);
 };
