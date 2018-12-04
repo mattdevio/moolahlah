073de3a08010f7c0075676af3cd13cd7f3497e10
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-04 02:35:23 -0700

    setup migration script promises

--- server/db/migrations/20181204023249_create_budget_record_table.js
+++ server/db/migrations/20181204023249_create_budget_record_table.js
@@ -1,8 +1,14 @@
+const tableName = 'budget_record';
 
 exports.up = function(knex, Promise) {
-  
+  return new Promise(async function(resolve) {
+    await knex.schema.createTable(tableName, (table) => {
+
+    });
+    resolve();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists(tableName);
 };
