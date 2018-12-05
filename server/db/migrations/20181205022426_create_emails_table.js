a5f07736aa8c128d8032ed6c558cfdb6d52e3ba2
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-05 02:38:54 -0700

    Added email table

---
+++ server/db/migrations/20181205022426_create_emails_table.js
@@ -0,0 +1,19 @@
+const tableName = 'emails';
+
+exports.up = function(knex, Promise) {
+  return new Promise(async function(resolve) {
+    await knex.schema.createTable(tableName, (table) => {
+      table.increments('id').primary();
+      table.binary('user_uuid', 16).notNullable();
+      table.date('sent_date').notNullable();
+      table.string('template_name').notNullable();
+      table.foreign('user_uuid').references('users.uuid').onDelete('cascade');
+      table.foreign('sent_date').references('calendar.db_date').onDelete('cascade');
+    });
+    resolve();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists(tableName);
+};
