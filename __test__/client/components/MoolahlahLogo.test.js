2fcfc675ea9915271a30dead8946f52bb54b5a37
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-11 00:41:59 -0700

    Fixing test so the compiler will stop warning me :)

--- __test__/client/components/MoolahlahLogo.test.js
+++ __test__/client/components/MoolahlahLogo.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 // Custom Imports
-import MoolahlahLogo from '@/components/MoolahlahLogo';
+import MoolahlahLogo from '@/components/atoms/MoolahlahLogo';
 
 describe('<MoolahlahLogo /> Component', () => {
 
