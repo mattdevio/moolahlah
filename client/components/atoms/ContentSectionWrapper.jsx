7de44c78395c5ab3f724f3721b7c0afa409c884c
Matt Greenberg <magreenberg@fullsail.edu>
2018-12-10 22:58:17 -0700

    Added content section wrapper

---
+++ client/components/atoms/ContentSectionWrapper.jsx
@@ -0,0 +1,15 @@
+// Vendor Imports
+import styled from 'styled-components';
+
+/**
+ * ContentSectionWrapper
+ * Gives the content section some padding as well as a center alignment on larger screens
+ */
+const ContentSectionWrapper = styled.div`
+  min-height: 100%;
+  width: 100%;
+  max-width: 90rem;
+  padding: 1rem;
+`;
+
+export default ContentSectionWrapper;
