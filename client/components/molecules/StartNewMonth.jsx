22737e6e515dd84e6ff0d1a1bbdf266c2eb2d31c
Matt Greenberg <magreenberg@fullsail.edu>
2019-01-20 14:24:03 -0700

    Updated start budget component to provide more information on what the budget designer does

--- client/components/molecules/StartNewMonth.jsx
+++ client/components/molecules/StartNewMonth.jsx
@@ -1,27 +1,23 @@
-/*----------  Vendor Imports  ----------*/
+// Vendor Imports
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { number } from 'prop-types';
 
-/*----------  Custom imports  ----------*/
+// Custom Imports
 import { displayMonths } from '@/bin/dateHelpers';
 
-/*===============================================
-=            StartNewMonth Component            =
-===============================================*/
 
 class StartNewMonth extends Component {
   render() {
-    const { currentMonth } = this.props;
+    const { currentMonth, currentYear } = this.props;
     return (
       <StartNewMonthContainer>
-        <NewMonthHeadline>
-          Nothing Here Yet...
-        </NewMonthHeadline>
-        <StartNewMonthButton>
-          { `Start ${displayMonths[currentMonth]}'s Budget` }
-        </StartNewMonthButton>
+        <WelcomeTitle>Welcome to the budget designer!</WelcomeTitle>
+        <WelcomeDirections>
+          This tool is used to help you plan and predict your monthly expenses. In this area, you will itemize your budget by breaking down each expense into categoriezed groups. These categorized groups will not only help you gain insight into your spending, but serve as a blueprint; which gives you an outline of possible expenses you might overlooked. When you are ready to start planning your budget for this month, press the button below!
+        </WelcomeDirections>
+        <StartBudgetButton>Start Planning for {displayMonths[currentMonth]} {currentYear}</StartBudgetButton>
       </StartNewMonthContainer>
     );
   }
@@ -29,35 +25,40 @@ class StartNewMonth extends Component {
 
 StartNewMonth.propTypes = {
   currentMonth: number.isRequired,
+  currentYear: number.isRequired,
 };
 
 const mapStateToProps = state => ({
   currentMonth: state.budget.currentMonth,
+  currentYear: state.budget.currentYear,
 });
 
 export default connect(mapStateToProps)(StartNewMonth);
 
-/*=====  End of StartNewMonth Component  ======*/
-
-
 const StartNewMonthContainer = styled.div`
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.5);
+  width: 100%;
   padding: 1.5rem;
-  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
-  max-width: 50rem;
-  margin: 0 auto;
 `;
 
-const NewMonthHeadline = styled.h3`
+const WelcomeTitle = styled.h2`
+  padding: 0;
   margin: 0 0 1rem 0;
-  font-size: 4rem;
-  letter-spacing: 0.3rem;
-  font-family: ${({ theme }) => theme.typeFont};
-  color: ${({ theme }) => theme.darkBlue};
-  text-transform: uppercase;
+  font-size: 3.5rem;
   text-align: center;
+  font-family: ${({theme}) => theme.typeFont};
+  letter-spacing: 0.2rem;
 `;
 
-const StartNewMonthButton = styled.button`
+const WelcomeDirections = styled.p`
+  padding: 0;
+  margin: 0 0 1rem 0;
+  font-family: ${({theme}) => theme.typeFont};
+  font-size: 2.2rem;
+  line-height: 3.5rem;
+`;
+
+const StartBudgetButton = styled.button`
   border: none;
   background: linear-gradient(
     ${({ theme }) => `${theme.lightBlue}, ${theme.darkBlue}`}
